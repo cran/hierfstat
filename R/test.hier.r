912264ebc60607c6fdab6591930038bfbf05d2e8
Jerome Goudet <jerome.goudet@unil.ch>
2005-03-14 00:00:00 +0000

    version 0.03-3

--- R/test.hier.r
+++ R/test.hier.r
@@ -4,17 +4,19 @@ function (data = data, test.lev, rand.unit, nperm = 100, ...)
     get.g <- function(x, data, ...) {
         g.stats.glob(data.frame(x, data), ...)$g.stats
     }
+    test.lev<-as.integer(factor(test.lev))
+    rand.unit<-as.integer(factor(paste(test.lev,"X",rand.unit,sep="")))
     x<-order(test.lev,rand.unit)
     data<-data.frame(data[x,])
     test.lev<-test.lev[x]
     rand.unit<-rand.unit[x]
-    runit<-paste(test.lev,rand.unit,sep="")
-    runit<-rep(1:dim(table(runit)),c(table(runit)))
-    nobs <- length(runit)
+#    runit<-paste(test.lev,rand.unit,sep="")
+#    runit<-rep(1:dim(table(runit)),c(table(runit)))
+#    nobs <- length(runit)
     perm.stat <- vector(length = nperm)
     perm.stat[nperm] <- get.g(test.lev, data, ...)
     for (i in 1:(nperm - 1)) {
-        perm.stat[i] <- get.g(test.lev, data[samp.between(runit), 
+        perm.stat[i] <- get.g(test.lev, data[samp.between(rand.unit), 
             ], ...)
     }
     list(g.star = perm.stat, p.val = sum(perm.stat >= perm.stat[nperm])/nperm)
@@ -26,21 +28,23 @@ function (data = data, within, test.lev, rand.unit, nperm = 100,
     get.g <- function(x, data, ...) {
         g.stats.glob(data.frame(x, data), ...)$g.stats
     }
-    
+    within<-as.integer(factor(within))
+    test.lev<-as.integer(factor(paste(within,"X",test.lev,sep="")))
+    rand.unit<-as.integer(factor(paste(within,"X",test.lev,"Y",rand.unit,sep="")))
     x<-order(within,test.lev,rand.unit)
     data<-data.frame(data[x,])
     within<-within[x]
     test.lev<-test.lev[x]
     rand.unit<-rand.unit[x]
-    tlev<-paste(within,test.lev,sep="")#
-    runit<-paste(tlev,rand.unit,sep="")#
-    tlev<-rep(1:dim(table(tlev)),c(table(tlev)))
-    runit<-rep(1:dim(table(runit)),c(table(runit)))
-    nobs <- length(test.lev)
+#    tlev<-paste(within,test.lev,sep="")#
+#    runit<-paste(tlev,rand.unit,sep="")#
+#    tlev<-rep(1:dim(table(tlev)),c(table(tlev)))
+#    runit<-rep(1:dim(table(runit)),c(table(runit)))
+#    nobs <- length(test.lev)
     perm.stat <- vector(length = nperm)
-    perm.stat[nperm] <- get.g(tlev, data, ...)
+    perm.stat[nperm] <- get.g(test.lev, data, ...)
     for (i in 1:(nperm - 1)) {
-        perm.stat[i] <- get.g(tlev, data[samp.between.within(inner.lev = runit, 
+        perm.stat[i] <- get.g(test.lev, data[samp.between.within(inner.lev = rand.unit, 
             outer.lev = within), ], ...)
     }
     list(g.star = perm.stat, p.val = sum(perm.stat >= perm.stat[nperm])/nperm)
@@ -51,7 +55,7 @@ function (data = data, level, nperm = 100, ...)
     get.g <- function(x, data, ...) {
         g.stats.glob(data.frame(x, data), ...)$g.stats
     }
-    nobs <- length(level)
+#    nobs <- length(level)
     perm.stat <- vector(length = nperm)
     perm.stat[nperm] <- get.g(level, data, ...)
     for (i in 1:(nperm - 1)) {
@@ -65,18 +69,19 @@ function (data = data, within, test.lev, nperm = 100, ...)
     get.g <- function(x, data, ...) {
         g.stats.glob(data.frame(x, data), ...)$g.stats
     }
-
+    within<-as.integer(factor(within))
+    test.lev<-as.integer(factor(paste(within,"X",test.lev,sep="")))
     x<-order(within,test.lev)
     data<-data.frame(data[x,])
     within<-within[x]
     test.lev<-test.lev[x]
-    tlev<-paste(within,test.lev,sep="")
-    tlev<-rep(1:dim(table(tlev)),c(table(tlev)))
-    nobs <- length(test.lev)
+#    tlev<-paste(within,test.lev,sep="")
+#    tlev<-rep(1:dim(table(tlev)),c(table(tlev)))
+#    nobs <- length(test.lev)
     perm.stat <- vector(length = nperm)
-    perm.stat[nperm] <- get.g(tlev, data, ...)
+    perm.stat[nperm] <- get.g(test.lev, data, ...)
     for (i in 1:(nperm - 1)) {
-        perm.stat[i] <- get.g(tlev, data[samp.within(within), 
+        perm.stat[i] <- get.g(test.lev, data[samp.within(within), 
             ], ...)
     }
     list(g.star = perm.stat, p.val = sum(perm.stat >= perm.stat[nperm])/nperm)
